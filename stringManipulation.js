960ee7c6c2207311c46302a163b173bec1d3a51e
onkarlondhe <onkarrameshlondhe@gmail.com>
2024-05-07 13:41:06 +0530

    #string manipulation task code added.

--- stringManipulation.js
+++ stringManipulation.js
@@ -2,13 +2,13 @@
 document.write("<h3>Capitalizing Words</h3>");
 
 
-let str = "hello guy's i am onkar.";
+let str = prompt("enter here : ");
  
- function capitalizeWords() {
+  function capitalizeWords() {
     let words = str.split(" ");
+	console.log(words);
     for (let i = 0; i < words.length; i++) {
         words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
-		
     }
     document.write(words.join(" "));
 }
@@ -18,19 +18,26 @@ capitalizeWords();
 document.write("<hr>");
 document.write("<br>");
 
+
+
 document.write("<h3>Capitalizing Each Word</h3>");
  
-function capitalizeAll(){
-	let str1 = "hello world...!";
-	let word1 = str1.split(" ");
+function capitalizeAll(givenString){
+	 
+	let convertingStringIntoArray = givenString.split("");
 	
-	for(let i=0; i<word1.length; i++){
-		word1[i] = word1[i].toUpperCase();
+	for(let i=0; i<convertingStringIntoArray.length; i++){
+		convertingStringIntoArray[i] = String.fromCharCode(convertingStringIntoArray[i].charCodeAt(0)-32);
 	}
-	document.write(word1.join(" "));
-}
+	
+	let desiredString = convertingStringIntoArray.join("");
+	console.log(desiredString);
+	document.write(desiredString);
 
-capitalizeAll();
+}
+capitalizeAll("krushna lavhare");
+
+ 
 
 document.write("<hr>");
 document.write("<br>");
@@ -39,11 +46,9 @@ document.write("<br>");
 document.write("<h3>Word Existence Check</h3>");
 
 
-function isExist(){
-	
-	let str3 = "jay hind jay maharashtra";
-	
-	let exist = str3.includes("maharashtra");
+function isExist(givenString){
+		
+	let exist = givenString.includes("ram");
 	
 	if(exist){
 		document.write("it is a part of string");
@@ -52,9 +57,223 @@ function isExist(){
 	}
 }
 
-isExist();
+isExist("jay shri ram");
  
  
 document.write("<hr>");
 document.write("<br>");
 
+
+document.write("<h3>Word Occurrence Count</h3>");
+
+function getOccurrenceCount(givenString, occurrenceWord){
+	
+	let word = givenString.split(" ");
+	let count = 0;
+	for(let i=0; i<word.length; i++){
+		if(word[i]===occurrenceWord){
+			count++;
+		}
+	}
+	
+	if(count>0){
+		return count;
+	}else{
+		return `'${occurrenceWord}' is not present in the given string.`;
+	}
+}
+
+let occurrenceCount = getOccurrenceCount("i am onkar and my full name is onkar ramesh londhe", "onkar");
+document.write(occurrenceCount);
+ 
+
+
+document.write("<hr>");
+document.write("<br>");
+
+
+
+
+
+document.write("<h3>Word Density Calculation:</h3>");
+
+function wordDensity(givenString, densityWord){
+	let words = givenString.split(" ");
+	let occurrenceCount = 0;
+	
+	for(let i=0; i<words.length; i++){
+		if(words[i]===densityWord){
+			 occurrenceCount++;
+		}
+	}
+	return (occurrenceCount/words.length)*100; 	
+}
+
+let densityCal = wordDensity("hare krushna hare krushna krushna krushna hare hare, hare ram hare ram ram ram hare hare", "krushna");
+document.write(densityCal);
+
+
+
+// document.write("<hr>");
+// document.write("<br>");
+
+
+// document.write("<h3>File Extension Extraction</h3>");
+
+ 
+ 
+
+document.write("<hr>");
+document.write("<br>");
+
+
+
+
+
+
+document.write("<h3>Removing Whitespace</h3>");
+
+function removeWhitespace(givenString){
+	let convertingStringIntoArray = givenString.split(" ");
+ 	let noWhiteSpace = [];
+	for(let i=0; i<convertingStringIntoArray.length; i++){
+		if(convertingStringIntoArray!=" "){
+			noWhiteSpace.push(convertingStringIntoArray[i]);
+		}
+	}
+	
+	let originalStr = noWhiteSpace.join(" ");
+	document.write(originalStr);
+}
+
+ removeWhitespace(" hello guy's ");
+ 
+document.write("<hr>");
+document.write("<br>");
+
+
+
+
+document.write("<h3>Reversing Words</h3>");
+
+let reverseString = " ";
+function reverseWords(givenString){
+	
+	for(let i=givenString.length-1; i>=0; i--){
+		 reverseString = reverseString + givenString[i];
+	}
+	document.write(reverseString);
+}
+
+reverseWords("Maharashtra");
+
+
+
+document.write("<hr>");
+document.write("<br>");
+
+
+
+
+document.write("<h3>Counting Vowels</h3>");
+
+function countVowels(givenString){
+	let str1 = givenString.split("");
+	let count = 0;
+	for(let i=0; i<str1.length; i++){
+		if(str1[i]==="a" || str1[i]==="e" || str1[i]==="i" || str1[i]==="o" || str1[i]==="u" || str1[i]==="A" || str1[i]==="E" || str1[i]==="I" || str1[i]==="O" || str1[i]==="U"){
+			count++;
+		}
+	}
+	document.write(count);
+}
+
+countVowels("Onkar Ramesh Londhe");
+
+
+document.write("<hr>");
+document.write("<br>");
+
+
+
+
+document.write("<h3>String Palindrome Check</h3>");
+
+function isPalindrome(givenString){
+	 
+	let reverseString = "";
+	for(let i=givenString.length-1; i>=0; i--){
+		reverseString = reverseString + givenString[i];
+	}
+	
+	if(givenString === reverseString){
+		document.write(`'${givenString}' is Palindrome.`);
+	}else{
+		document.write(`'${givenString}' is not Palindrome.`);
+	}
+}
+
+isPalindrome("racecar");
+
+
+document.write("<hr>");
+document.write("<br>");
+
+
+
+
+document.write("<h3>String Truncation</h3>");
+
+function truncateString(givenString){
+	let truncateString = givenString+"...";
+	// let padStr = givenString.padEnd(givenString.length+5, ".");
+	document.write(truncateString);
+}
+
+truncateString("Hello World");
+
+
+document.write("<hr>");
+document.write("<br>");
+
+
+
+
+document.write("<h3>String Masking</h3>");
+
+// function maskString(givenString){
+	
+	// let slice = givenString.split();
+	 
+	// for(i=0; i<slice.length; i++){
+		// slice = slice + "*";
+	// }
+	
+	// document.write(slice);
+// }
+
+// maskString("97605093");
+
+
+document.write("<hr>");
+document.write("<br>");
+
+
+
+
+document.write("<h3>Email Validation</h3>");
+
+function validateEmail(givenEmail){
+	
+	let regex = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/; 
+	let emailValidation = givenEmail.match(regex);
+	
+	if(emailValidation!=null){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+let result = validateEmail("onkar@gmail.com");
+document.write(result);
